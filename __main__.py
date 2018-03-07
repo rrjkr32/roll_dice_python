beaa8b3200f028f41fcd35632c767757d213c4da
rrjkr32 <rrjkr32@gmail.com>
2018-03-07 20:04:45 +0530

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,18 @@
+from random import *
+
+def roll():
+    val=randint(1,6)
+    return val
+
+def main():
+    flag=True
+
+    while flag:
+        ip=input("Press enter to roll and 'q' to quit \n")
+        if ip.lower()!='q':
+            op=roll()
+            print("You got a",op)
+        else:
+            flag=False
+            print("Thanks for playing,Bye!")
+main()
